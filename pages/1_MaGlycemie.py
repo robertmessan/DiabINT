1656925b5ca4e8d2fb939bc2a0e37017fc4d68cf
R_MESSAN <110102072+robertmessan@users.noreply.github.com>
2024-03-23 04:17:50 +0100

    Update and rename 1_Diabetes_Predict.py to 1_MaGlycemie.py

--- pages/1_MaGlycemie.py
+++ pages/1_MaGlycemie.py
@@ -205,6 +205,20 @@ if submit:
     st.header(f"Probability of being positive:  **:red[{prediction}]**")
 
 
+hide_st_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
+
+
+st.markdown("[Retourner sur la page d'accueil](https://github.com/robertmessan)\n\n\n\n")
+st.markdown("\n\n\n\n\n[Kossi Robert MESSAN](https://www.linkedin.com/in/kossi-robert-messan-252954223/)")
 
 
 
