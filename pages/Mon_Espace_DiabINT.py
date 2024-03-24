45d29d60d2f1dfd0414eca7f2346bfa027c7ccb3
R_MESSAN <110102072+robertmessan@users.noreply.github.com>
2024-03-24 15:05:34 +0100

    Update Mon_Espace_DiabINT.py

--- pages/Mon_Espace_DiabINT.py
+++ pages/Mon_Espace_DiabINT.py
@@ -23,6 +23,16 @@ st.set_page_config(layout="wide")
 
 
 def Diabetes_Predict():
+    if st.sidebar.button('LOGOUT🔁'):
+        # Clear user data from session state
+        # st.session_state.pop("user_data", None)
+        # Login_and_Registration.loginAndRegister()
+        Login_and_Registration.loginAndRegister()
+        st.session_state.logged_in = False
+        st.session_state['user_data'] = None
+        
+        st.success('Logged out successfully.')
+        return False
     # st.button('LOGOUT')
     # Display user data
     st.write(st.session_state["user_data"])
@@ -44,19 +54,10 @@ def Diabetes_Predict():
     # st.sidebar.header(f'Welcome!!! {username}')
     # st.sidebar.markdown(msg,unsafe_allow_html=True)
     
-    if st.sidebar.button('LOGOUT🔁'):
-        # Clear user data from session state
-        # st.session_state.pop("user_data", None)
-        # Login_and_Registration.loginAndRegister()
-        Login_and_Registration.loginAndRegister()
-        st.session_state.logged_in = False
-        st.session_state['user_data'] = None
-        
-        st.success('Logged out successfully.')
-        return False
+
         
 
-        st.stop()
+        # st.stop()
     
     st.header('Diabetes Prediction System')
     name = st.text_input("Enter your Name:",value=username)
