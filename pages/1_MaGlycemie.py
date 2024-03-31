8d49bae5e3d3439de442f6d839c9d9b0771d7252
R_MESSAN <110102072+robertmessan@users.noreply.github.com>
2024-03-31 16:28:54 +0200

    Update 1_MaGlycemie.py

--- pages/1_MaGlycemie.py
+++ pages/1_MaGlycemie.py
@@ -211,7 +211,7 @@ if submit:
     elif prediction == "Risque Faible":
         st.header(f"Le résultat du diagnostic(probabilité d'être diabétique):  **:green[{prediction}]**")
 st.write(data.iloc[0])
-st.write(data['age'].iloc[0] > age_threshold and data['BMI'].iloc[0] > bmi_threshold and data['FamilyHistory'].iloc[0] > family_history_threshold and data['physical_activity'].iloc[0] < physical_activity_threshold and data['systolic_pressure'].iloc[0] > systolic_pressure and data['diastolic_pressure'].iloc[0] > diastolic_pressure)
+st.write(data['age'].iloc[1] > age_threshold and data['BMI'].iloc[1] > bmi_threshold and data['FamilyHistory'].iloc[1] > family_history_threshold and data['physical_activity'].iloc[1] < physical_activity_threshold and data['systolic_pressure'].iloc[1] > systolic_pressure and data['diastolic_pressure'].iloc[1] > diastolic_pressure)
 hide_st_style = """
     <style>
     #MainMenu {visibility: hidden;}
