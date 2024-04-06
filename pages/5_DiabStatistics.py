73c841acc17e38cfed56b10a3058ab5e8df15827
R_MESSAN <110102072+robertmessan@users.noreply.github.com>
2024-04-06 17:18:34 +0200

    Update 5_DiabStatistics.py

--- pages/5_DiabStatistics.py
+++ pages/5_DiabStatistics.py
@@ -18,7 +18,7 @@ st.markdown(html_temp,unsafe_allow_html=True)
 
 st.subheader('Carte du monde des Spécialistes du diabète🌎')
 data1=pd.read_csv("data/dataR.csv")
-st.write(data1.columns)
+st.write(data1['Profession'].unique())
 #, 'Spécialiste en médecine interne'
 professions_diabete = ['Endocrinologue-diabétologue', 'Cardiologue',
                        'Gastro-entérologue et hépatologue', 'Ophtalmologiste', 'Néphrologue', 'Neurologue',
